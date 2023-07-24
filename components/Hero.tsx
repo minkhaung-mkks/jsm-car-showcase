bb66c08870368c8539339d5a19d5c3b86fb05ecb
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-24 18:32:15 +0630

    Switched to Client component due to error

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,11 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
+import CustomBtn from './CustomBtn'
 const Hero = () => {
+    const handleScroll=()=>{
+
+    }
     return (
         <div className="hero">
             <div className="flex-1 pt-36 padding-x">
@@ -10,6 +15,17 @@ const Hero = () => {
                 <p className="hero__subtitle">
                     Streamline your car rental experience with our effortless booking process.
                 </p>
+                <CustomBtn
+                    title='Explore Cars'
+                    containerStyles='bg-primary-blue text-white rounded-full mt-10'
+                    handleClick={handleScroll}
+                />
+            </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src='/hero.png' alt='hero Image of a car' fill className='object-contain' />
+                    <div className="hero__image-overlay"></div>
+                </div>
             </div>
         </div>
     )
