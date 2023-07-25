70acfde5a23ae18dd3d7e0ded2336b7d7ccfd632
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-25 17:19:14 +0630

    Fixed A few Css typos

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,9 +34,10 @@ const Footer = () => {
                         </div>
                     ))}
                 </div>
+            </div>
                 <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
                     <p>@2023 CarHub. All Rights Reserved</p>
-                    <div className="footer__copyright-link">
+                    <div className="footer__copyrights-link">
                         <Link
                             href={'/'}
                             className='text-gray-500'
@@ -51,7 +52,6 @@ const Footer = () => {
                         </Link>
                     </div>
                 </div>
-            </div>
         </footer>
     )
 }
