1e026ed9beda864469e3671c3a18db4ea00a4e08
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-26 17:07:56 +0630

    Base Filter and Catalogue Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 import Image from 'next/image'
 
-import {Hero} from '@/components'
+import {CustomFilters, Hero, Searchbar} from '@/components'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero/>
+      <div className="mt-12 padding-x padding-y max-width" id='discover'>
+          <div className="home__text-container">
+            <h1 className="text-4x1 font-extrabold">
+                Available Cars
+            </h1>
+            <p>Explore our best options</p>
+          </div>
+          <div className="home__fliters">
+            <Searchbar/>
+            <div className="home__filter-container">
+              <CustomFilters/>
+              <CustomFilters/>
+            </div>
+          </div>
+      </div>
     </main>
   )
 }
