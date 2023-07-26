eabb591453f773eef2b500670b1bf35b2fcfbf41
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-26 17:08:28 +0630

    Created CustomFilter and Searchbar components

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,7 @@ import Hero from "./Hero"
 import CustomBtn from "./CustomBtn"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import CustomFilters from "./CustomFilters"
+import Searchbar from "./Searchbar"
 
-export { Hero, CustomBtn, Navbar, Footer }
+export { Hero, CustomBtn, Navbar, Footer, CustomFilters, Searchbar }
