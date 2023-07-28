3784f2a537d135a65b6184532a0eb2ab941411f7
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-28 18:09:45 +0630

    Removed Codes

--- utlis/index.ts
+++ utlis/index.ts
@@ -1,20 +1,3 @@
-const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars"
-const options = {
-	method: "GET",
-	params: { model: "corolla" },
-	headers: {
-		"X-RapidAPI-Key": "cee6710e73mshc9ea9d056f0d42dp158d84jsn1ba4fb13fd4f",
-		"X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-	},
-}
-
-try {
-	const response = await fetch(url, options)
-	const result = await response.text()
-} catch (error) {
-	console.error(error)
-}
-
 export async function fetchCars() {
 	const headers = {
 		"X-RapidAPI-Key": "cee6710e73mshc9ea9d056f0d42dp158d84jsn1ba4fb13fd4f",
