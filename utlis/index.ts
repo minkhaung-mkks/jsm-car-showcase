912e37c9021cad4386fe574b28521770756d6fc9
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-28 18:12:42 +0630

    API dont work without addtional perimeters

--- utlis/index.ts
+++ utlis/index.ts
@@ -3,7 +3,7 @@ export async function fetchCars() {
 		"X-RapidAPI-Key": "cee6710e73mshc9ea9d056f0d42dp158d84jsn1ba4fb13fd4f",
 		"X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
 	}
-	const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars"
+	const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla"
 	const response = await fetch(url, { headers: headers })
 	const result = await response.json()
 
