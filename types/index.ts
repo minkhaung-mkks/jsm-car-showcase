8e99d0d66b0d6d01329f5afe140d1edfb5835a26
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-28 18:19:44 +0630

    Car Card Interface

--- types/index.ts
+++ types/index.ts
@@ -11,3 +11,13 @@ export interface SearchManufacturerProps {
 	manufacturer: string
 	setManufacturer: (manufacturer: string) => void
 }
+
+export interface CarCardProps {
+	model: string
+	make: string
+	mpg: number
+	transmission: string
+	year: number
+	drive: string
+	cityMPG: number
+}
