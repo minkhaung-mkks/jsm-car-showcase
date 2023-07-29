a5d3d3244c10b50623053ea40daf7dc2c65b7596
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-29 17:18:24 +0630

    Added Icon and TextStyles to the component

--- components/CustomBtn.tsx
+++ components/CustomBtn.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { CustomBtnProps } from '@types'
+import Image from 'next/image';
 
-const CustomBtn = ({title,containerStyles, handleClick,btnType}: CustomBtnProps) => {
+const CustomBtn = ({title,containerStyles, handleClick,btnType,rightIcon, textStyles}: CustomBtnProps) => {
     return (
         <button
             disabled={false}
@@ -9,9 +10,19 @@ const CustomBtn = ({title,containerStyles, handleClick,btnType}: CustomBtnProps)
             className={`custom-btn ${containerStyles}`}
             onClick={handleClick}
         >
-            <span className={"flex-1"}>
+            <span className={`flex-1 ${textStyles}`}>
                 {title}
             </span>
+            {rightIcon && (
+                <div>
+                    <Image
+                        src={rightIcon}
+                        alt='icon'
+                        fill
+                        className='object-contain'
+                    />
+                </div>
+            )}
         </button>
     )
 }
