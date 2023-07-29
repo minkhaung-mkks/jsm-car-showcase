c4d3a3eebd0fb8502bb9741076a840859399f241
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-29 17:32:21 +0630

    Changes to margins and gap

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from 'next/image'
 import { CarProps } from '@types'
 import { calculateCarRent } from '@utlis'
@@ -12,13 +13,13 @@ const CarCard = ({car}:CarCardProps) => {
     const carRent = calculateCarRent(city_mpg,year);
     const [isOpen, setIsOpen] = useState(false)
     return (
-        <div className='car-card group'>
+        <div className='car-card group '>
             <div className="car-card__content">
                 <h2 className="car-card__content-title">
                     {make} {model}
                 </h2>
             </div>
-            <p className='flex mt-6 text-[32px] font-extrabold'>
+            <p className='flex mt-1 text-[32px] font-extrabold'>
                 <span className='self-center text-[18px] leading-[17px] font-semibold'>
                     $
                 </span>
@@ -29,19 +30,20 @@ const CarCard = ({car}:CarCardProps) => {
                     /day
                 </span>
             </p>
-            <div className="relative w-full h-40 my-3 object-contain">
+            <div className="relative w-full h-40 mt-6 xl:mt-8 object-contain">
                 <Image 
                 src='/hero.png' 
                 alt='car model' 
                 fill priority 
                 className='object-contain'/>
             </div>
-            <div className="relative w-full mt-2 flex">
+            <div className="relative w-full mt-6 xl:mt-9 flex">
                 <div className="flex group-hover:invisible w-full justify-between text-gray">
                     <div className="flex flex-col justify-center items-center gap-2">
                         <Image 
                             src='/steering-wheel.svg'
                             alt='steering wheel icon'
+                            width={20} height={20}
                         />
                         <p className='text-[14px]'>
                             {transmission === 'a' ? 'Automatic' : 'Manual'}
@@ -51,6 +53,7 @@ const CarCard = ({car}:CarCardProps) => {
                         <Image 
                             src='/tire.svg'
                             alt='Tire icon'
+                            width={20} height={20}
                         />
                         <p className='text-[14px]'>
                             {drive.toUpperCase()}
@@ -60,20 +63,21 @@ const CarCard = ({car}:CarCardProps) => {
                         <Image 
                             src='/gas.svg'
                             alt='Gas tank icon'
+                            width={20} height={20}
                         />
                         <p className='text-[14px]'>
                             {city_mpg} MPG
                         </p>
                     </div>
-                    <div className="car-card__btn-container">
+                </div>
+                <div className="car-card__btn-container">
                         <CustomBtn
                             title='View More'
-                            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+                            containerStyles='w-full py-[16px] rounded-full bg-primary-blue cursor-pointer'
                             textStyles = 'text-white text-[14px] leading-[17px] font-bold'
                             rightIcon='/right-arrow.svg'
                             handleClick={()=> setIsOpen(true)}
                         />
-                    </div>
                 </div>
             </div>
         </div>
