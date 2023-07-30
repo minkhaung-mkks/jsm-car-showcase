f1be415d74af3f9b24cc06a3e871a8e7fec13805
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-30 17:42:36 +0630

    Backdrop

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,15 +1,33 @@
+import { Dialog, Transition } from '@headlessui/react'
 import { CarProps } from '@types'
-import React from 'react'
+import React, { Fragment } from 'react'
+
 
 interface CarDetailsProps{
     isOpen: boolean
-    closeModel: ()=> void
+    closeModal: ()=> void
     car:CarProps
 }
 
-const CarDetails = ({isOpen,closeModel,car}: CarDetailsProps)  => {
+const CarDetails = ({isOpen,closeModal: closeModal,car}: CarDetailsProps)  => {
   return (
-    <div>CarDetails</div>
+    <>
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className={'relative z-10'} onClose={closeModal}>
+                <Transition.Child
+                    as={Fragment}
+                    enter='ease-out duration-300'
+                    enterFrom='opacity-0'
+                    enterTo='opacity-100'
+                    leave='ease-in duration-200'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25"></div>
+                </Transition.Child>
+            </Dialog>
+        </Transition>
+    </>
   )
 }
 
