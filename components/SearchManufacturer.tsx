5ec79ff7784207e4e981e32b8b587c57ca75050a
Min Khaung <minkhaung.mkks@gmail.com>
2023-07-31 17:04:39 +0630

    Fixed Erasing the manufacturers resulting in just defaulting back to the selected one

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -15,7 +15,10 @@ const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturer
       .replace(/\s+/g, "")
       .includes(query.toLowerCase().replace(/\s+/g, ""))
     ))
-
+  const searchForMakers = (maker:string) =>{
+    setQuery(maker)
+    setManufacturer(maker)
+  }
   return (
     <div className="search-manufacturer">
       <Combobox value={manufacturer} onChange={setManufacturer}>
@@ -32,7 +35,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturer
             <Combobox.Input className='search-manufacturer__input'
               placeholder='Volkswagon'
               displayValue={(manufacturer:string)=>manufacturer}
-              onChange={(e)=>setQuery(e.target.value)}
+              onChange={(e)=>searchForMakers(e.target.value)}
             />
             <Transition
               as={Fragment}
