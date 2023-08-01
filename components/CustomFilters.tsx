d1d87bc704b80b29bf1b1be1aae9d819833e885b
Min Khaung <minkhaung.mkks@gmail.com>
2023-08-01 16:45:14 +0630

    Added All to filters to reset

--- components/CustomFilters.tsx
+++ components/CustomFilters.tsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import {useState,Fragment} from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { Listbox, Transition } from '@headlessui/react'
+import { filterProps } from '@types'
 
-const CustomFilters = () => {
+const CustomFilters = ({title, options} : filterProps) => {
+  const [selected, setSelected] = useState(options[0])
   return (
-    <div>CustomFilters</div>
+    <div className="w-fit">
+      <Listbox
+        value={selected}
+        onChange={(e)=>setSelected(e)}
+      >
+        <div className="relative w-fit z-10">
+          <Listbox.Button className={'custom-filter__btn'}>
+            <span className='truncate block '>{selected.title}</span>
+            <Image
+              src='/chevron-up-down.svh'
+              alt='Chervon Up Down'
+              width={20}
+              height={20}
+              className={'ml-4 object-contain'}
+            />
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <Listbox.Options
+              className="custom-filter__options"
+            >
+            <Listbox.Option
+                value={'All'}
+                className={({active})=>`relative cursor-default select-none py-2 px-4 ${active ? 'bg-primary-blue text-white': 'text-gray-900'}`}
+              >
+                {({selected})=>(
+                  <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                    All
+                  </span>
+                )}
+              </Listbox.Option>
+              {options.map((option)=>(
+                <Listbox.Option
+                  key={option.title}
+                  value={option}
+                  className={({active})=>`relative cursor-default select-none py-2 px-4 ${active ? 'bg-primary-blue text-white': 'text-gray-900'}`}
+                >
+                  {({selected})=>(
+                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                      {option.title}
+                    </span>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
   )
 }
 
